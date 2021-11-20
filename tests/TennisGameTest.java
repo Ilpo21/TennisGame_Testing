c3df986d8e0179e739ce4085e34d7ef87f3043ab
Ilpo21 <93974495+Ilpo21@users.noreply.github.com>
2021-11-20 16:24:33 +0200

    Modattu

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -1,58 +1,10 @@
-// This implementation is used for practicing unit tests. 
-// NOTE THAT it may contain bugs
-// Write unit tests in TennisGameTest.java and try to find the errors in the code
+import static org.junit.Assert.*;
 
-public class TennisGame {
-	private int player1Points;
-	private int player2Points;
+import org.junit.Test;
+
+public class TennisGameTest {
 	
-	private boolean gameEnded;
-	
-	public TennisGame() {
-		player1Points = 0;
-		player2Points = 0;
-		gameEnded = false ;
-	}
-	
-	private void checkGameEnded() {
-		if (player1Points>=4 && player1Points-player2Points>=2)
-			gameEnded = true;
-		else if (player2Points>=4 && player2Points-player1Points>=2)
-			gameEnded = true;
-	}
-	
-	private String getScore(int points) {
-		switch (points)	{
-		case 0: return "love";
-		case 1: return "15" ;
-		case 2: return "30" ;
-		case 3: return "40";
-		default: return "40" ;
-		} 		
-	}
-	
-	public void player1Scored() throws TennisGameException {
-		if (gameEnded) {
-			throw new TennisGameException();
-		}
-		else {
-			player1Points++;
-			checkGameEnded();
-		}			
-	}
-	
-	public void player2Scored() throws TennisGameException {
-		if (gameEnded) {
-			throw new TennisGameException();
-		}
-		else {
-			player2Points++;
-			checkGameEnded();
-		}			
-	}
-	
-	public String getScore() {
-// Here is the format of the scores:
+// Here is the format of the scores: "player1Score - player2Score"
 // "love - love"
 // "15 - 15"
 // "30 - 30"
@@ -66,28 +18,140 @@ public class TennisGame {
 // "player2 has advantage"
 // "player1 wins"
 // "player2 wins"
+	@Test
+	public void testTennisGame_Start() {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Initial score incorrect", "love - love", score);		//is the score 0 at the start?
+	}
+	
+	@Test
+	public void testTennisGame_EahcPlayerWin4Points_Score_Deuce() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
 		
-			String player1Score = getScore(player1Points);
-			String player2Score = getScore(player2Points);
-			
-			if (gameEnded) {
-				if (player1Points > player2Points)
-					return "player1 wins";
-				else
-					return "player2 wins";
-			}
-			
-			if (player1Points >= 4 && player1Points == player2Points)
-				return "deuce";
-			
-			if (player1Points >= 4 && player1Points - player2Points == 1)
-				return "player1 has advantage";
-			
-			if (player2Points >= 4 && player2Points - player1Points == 1)
-				return "player2 has advantage";			
-			//added "=" to row 86 before the number 4 it was "player2Points > 4 && player2Points"
-			
-			return  player1Score + " - " + player2Score ;
-			// switched player1score and player2score places. player 1 score was on the left now on the right and vice versa.
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Tie score incorrect", "deuce", score);		//is the score tied when both have 4 points
+	}
+	
+	@Test (expected = TennisGameException.class)
+	public void testTennisGame_Player1WinsPointAfterGameEnded_ResultsException() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		//Act
+		// This statement should cause an exception
+		game.player1Scored();			
+	}		
+	
+	@Test (expected = TennisGameException.class)
+	public void testTennisGame_Player2WinsPointAfterGameEnded_ResultsException() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		//Act
+		// This statement should cause an exception
+		game.player2Scored();			
+	}		
+
+	@Test 
+	public void testTennisGame_tie1_ResultsException() throws TennisGameException{
+	TennisGame game = new TennisGame();
+
+	game.player1Scored();
+	game.player1Scored();
+
+	game.player2Scored();
+	game.player2Scored();
+
+	String score = game.getScore();
+	assertEquals("Points wrong", "30 - 30",score); //test when game is 30-30
+	}
+	
+	@Test
+	public void testTennisGame_tie2_ResultsEsception() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		
+		game.player2Scored();
+		
+		String score = game.getScore();
+		assertEquals("Points wrong", "15 - 15", score); //test when game is  15-15
+		
+	}
+	
+	@Test
+	public void testTennisGame_Advantage1_ResultsException() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		
+		game.player2Scored();
+		
+		game.player1Scored();
+		
+		game.player2Scored();
+		
+		game.player1Scored();
+		
+		String score = game.getScore();
+		assertEquals("Points wrong", "player1 has advantage", score); //Test when player 1 has advantage
+		
+	}
+	
+	@Test
+	public void testTennisGame_Advantage2_ResultsException() throws TennisGameException{
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		
+		game.player2Scored();
+		
+		game.player1Scored();
+		
+		game.player2Scored();
+		
+		game.player2Scored();
+		
+		String score = game.getScore();
+		assertEquals("Points wrong", "player2 has advantage", score); //Test when player 2 has advantage
+		
 	}
 }
+
